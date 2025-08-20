0b93c4302e8c568d884d17e25dc5d589678b5203
Nikita Shebalin <shebalin13@gmail.com>
2025-08-20 13:54:39 +0300

    refactor: clean up comments and improve code readability across multiple files

--- android/src/main/kotlin/com/shebnik/flutter_screen_time/FlutterScreenTimePlugin.kt
+++ android/src/main/kotlin/com/shebnik/flutter_screen_time/FlutterScreenTimePlugin.kt
@@ -10,10 +10,6 @@ import io.flutter.plugin.common.MethodChannel.Result
 class FlutterScreenTimePlugin :
     FlutterPlugin,
     MethodCallHandler {
-    // The MethodChannel that will the communication between Flutter and native Android
-    //
-    // This local reference serves to register the plugin with the Flutter Engine and unregister it
-    // when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
--- lib/flutter_screen_time_method_channel.dart
+++ lib/flutter_screen_time_method_channel.dart
@@ -3,9 +3,7 @@ import 'package:flutter/services.dart';
 
 import 'package:flutter_screen_time/flutter_screen_time_platform_interface.dart';
 
-/// An implementation of [FlutterScreenTimePlatform] that uses method channels.
 class MethodChannelFlutterScreenTime extends FlutterScreenTimePlatform {
-  /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('flutter_screen_time');
 
